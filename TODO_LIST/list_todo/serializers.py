ee90d904c0e05f1799b0b7b2bf423691fada2268
xah-t <milaaman@rambler.ru>
2021-04-10 11:31:27 +0300

    add NoteSerializer

--- TODO_LIST/list_todo/serializers.py
+++ TODO_LIST/list_todo/serializers.py
@@ -3,9 +3,21 @@ from rest_framework import serializers
 from .models import Note
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    """Автор статьи"""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'date_joined')
+
+
 class NotesSerializer(serializers.ModelSerializer):
     """Заметки"""
     author = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'message', 'date_add', 'author', )
 
 
 
